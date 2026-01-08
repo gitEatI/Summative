f628ca305f7eb4e06246760d1941a9edaca47c0e
imini1 <imini1@ocdsb.ca>
2026-01-08 13:24:10 -0500

    Yesirsky

--- src/Gui.java
+++ src/Gui.java
@@ -1,4 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
+
 public class Gui extends JFrame {
 
+    public Gui() {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        setUndecorated(true);
+
+        // Make it full screen
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        gd.setFullScreenWindow(this);
+    }
 }
